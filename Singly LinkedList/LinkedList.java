0afd5472dacc4506af082c29d3d2df5e7bd0c8c6
Uttam kumar <72701081+helper-uttam@users.noreply.github.com>
2021-11-18 22:46:05 +0530

    Add files via upload
    Adding singly LinkedList Code

---
+++ Singly LinkedList/LinkedList.java
@@ -0,0 +1,43 @@
+public class LinkedList{
+    
+    private Node head;
+    private Node tail;
+    private int size;
+    
+    public LinkedList(){
+        this.size = 0;
+    }
+    
+    public void insertAtFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        
+        if(tail == null){
+            tail = head;
+        }
+        size++;
+    }
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    
+    private class Node{
+        
+       private int value;
+       private Node next;
+        
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+    
+}
---
+++ Singly LinkedList/Main.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.insertAtFirst(5);
+		list.insertAtFirst(56);
+		list.insertAtFirst(54);
+		list.display();
+	}
+}
