bd4f32f741c7d0305258c8aba228d0f50f8cf2e9
Uttam kumar <72701081+helper-uttam@users.noreply.github.com>
2021-11-19 20:06:37 +0530

    Update Main.java
    calling remove from first function

--- Singly LinkedList/Main.java
+++ Singly LinkedList/Main.java
@@ -4,9 +4,14 @@ public class Main
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.insertAtFirst(5);
-		list.insertAtFirst(56);
-		list.insertAtFirst(54);
+		list.insertAtFirst(4);
+		list.insertAtFirst(2);
+		
 		list.insertAtBack(58);
+		list.insertAtBack(59);
+		
+		list.remove_first();
+			
 		list.display();
 	}
 }
